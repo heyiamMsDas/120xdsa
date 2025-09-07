2b4d332d0db949d63aab3cb75f8f82f20258faaa
Madhusudan Das <msdas7476@gmail.com>
2025-09-07 14:15:18 +0530

    Update redlight.cpp

--- Day07/redlight.cpp
+++ Day07/redlight.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    
+     int test ;
+    cin>>test ;
+    while(test--){
+        int N , K ;
+        cin>>N >> K ;
+        int count = 0 ;
+        while(N--){
+            int x ;
+            cin>>x ;
+            if(x > K) count++ ;
+        }
+        cout<<count<<endl;     
+    }
+}
 
